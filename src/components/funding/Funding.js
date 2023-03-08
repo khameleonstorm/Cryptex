621ac481bf74f3fdfe9b39a81ef2d3b8ab3e22f1
khameleonstorm <khameleonstorm@gmail.com>
2023-03-08 22:49:02 +0100

    g

--- src/components/funding/Funding.js
+++ src/components/funding/Funding.js
@@ -1,13 +1,25 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, Fragment } from "react";
 import { VscCopy } from "react-icons/vsc";
 import styles from "./Funding.module.css";
 import QR from "../../assets/QR.jpg";
+import { useNavigate } from "react-router-dom";
+import ImageUploading from 'react-images-uploading';
 
 
 export default function Funding() {
   const [copy, setCopy] = useState(false);
   const [copySuccess, setCopySuccess] = useState(false);
+  const [proof, setProof] = useState(false);
+  const [images, setImages] = useState([]);
   const textAreaRef = useRef(null);
+  const navigate = useNavigate();
+
+  const handleImage = (imageList, addUpdateIndex) => {
+    // data for submit
+    console.log(imageList, addUpdateIndex);
+    setImages(imageList);
+  };
+
 
 
   const handleClick =  (e) => {
@@ -15,7 +27,8 @@ export default function Funding() {
   }
 
   const btnClick =  (e) => {
-    if (e === "pay") window.location = "https://wa.me/message/X7T6XZWN4MNVL1"
+    if (e === "back") navigate("/dashboard")
+    if (e === "upload") setProof(true)
   }
   
 
@@ -45,9 +58,13 @@ export default function Funding() {
     }
   }
 
+  const submitImage = () => {
+    console.log(images[0])
+  }
+
   return (
     <div className={styles.container}>
-      {
+      {!proof &&
         <div className={styles.CryptoFund}>
           <div className={styles.qr}>
               <div id="qr" className={styles.imgCtn}></div>
@@ -72,13 +89,36 @@ export default function Funding() {
             <p>Send only <span>USDT(TRC 20) </span>to this address, sending any other coin may result to permanent loss</p>
           </div>
 
-          <button className="bigBtn full" style={{...overwrite}} onClick={() => btnClick("pay")}>
-            Pay via CTM exchange <span style={{...span}}>&rarr;</span>
+          <button className="bigBtn full" style={{...overwrite}} onClick={() => btnClick("back")}>
+          <span style={{...span2}}>&larr;</span> Back To Dashboard
           </button>
           <button className="bigBtn full" style={{...overwrite}} onClick={() => btnClick("upload")}>
             Upload Payment Proof <span style={{...span}}>&rarr;</span>
           </button>
         </div>
+      }
+
+      {proof &&
+        <ImageUploading multiple value={images} onChange={handleImage} maxNumber={1} dataURLKey="data_url" >
+        {({ imageList, onImageUpload, onImageUpdate, onImageRemove, isDragging, dragProps }) => (
+          // write your building UI
+          <div className={styles.uploadCtn} style={imageList.length > 0 ? {...someCtnStyles} : undefined}  {...dragProps}>
+            <button style={isDragging ? { color: 'red' } : undefined} onClick={onImageUpload}> Click or Drop here </button>
+            {imageList.map((image, i) => (
+              <Fragment key={i}>
+                <div className={styles.imgCtn2}>
+                  <img src={image['data_url']} alt="proof"/>
+                <div className={styles.btns2}>
+                  <button className="bigBtn full" style={{...overwrite}} onClick={() => onImageUpdate(i)}>Update</button>
+                  <button className="bigBtn full" style={{...overwrite}} onClick={() => onImageRemove(i)}>Remove</button>
+                  <button className="bigBtn full" style={{...overwrite}} onClick={submitImage}>Upload</button>
+                </div>
+                </div>
+              </Fragment>
+            ))}
+          </div>
+        )}
+      </ImageUploading>
       }
 
     </div>
@@ -95,4 +135,14 @@ export default function Funding() {
   const span = {
     fontSize: "1.5em",
     paddingLeft: "1em",
+  }
+
+  const span2 = {
+    fontSize: "1.5em",
+    paddingRight: "1em",
+  }
+
+  const someCtnStyles = {
+    minHeight: "auto",
+    border: "none",
   }
