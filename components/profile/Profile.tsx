e6cc1b14f9b0c921c5808d6e738f877a677a7f53
khameleonstorm <khameleonstorm@gmail.com>
2023-03-16 00:21:35 +0100

    profile

--- components/profile/Profile.tsx
+++ components/profile/Profile.tsx
@@ -59,30 +59,12 @@ export default function Profile({ document }: Props | any): JSX.Element {
             <h1>${total}</h1>
           </div>
         </div>
-        <div className={s.referral}>
-          <div className={s.referralCode}>
-            <p>Referral Code</p>
-            <h1>{user.displayName}</h1>
-          </div>
-        </div>
         <div className={s.moreDetails}>
           <h1>Profile Info</h1>
           <div className={s.details}>
-            <p>
-              Full Name: <span>{profile?.fullName}</span>
-            </p>
-            <p>
-              Email: <span>{profile?.email}</span>
-            </p>
-            <p>
-              Phone Number: <span>{profile?.phoneNumber}</span>
-            </p>
-            <p>
-              Country: <span>{profile?.country}</span>
-            </p>
-            <p>
-              UserID: <span>{profile?.uid}</span>
-            </p>
+            <p>Full Name: <span>{profile?.fullName}</span> </p>
+            <p>Email: <span>{profile?.email}</span></p>
+            <p>Phone Number: <span>{profile?.phoneNumber}</span></p>
           </div>
         </div>
       </div>
