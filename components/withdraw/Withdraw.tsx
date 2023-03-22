0b30b899823aae30ea8a7d5e42958554c4ed661f
khameleonstorm <khameleonstorm@gmail.com>
2023-03-22 15:31:10 +0100

    with

--- components/withdraw/Withdraw.tsx
+++ components/withdraw/Withdraw.tsx
@@ -9,8 +9,9 @@ export default function Withdraw({userDoc}: any | {doc: any}) {
   const [wallet, setWallet] = useState<string>('')
   const [error, setError] = useState<null | string>(null)
   const [isPending, setIsPending] = useState(false)
+  const [isSuccess, setIsSuccess] = useState(false)
 
-  const handleWithdraw = (e: any) => {
+  const handleWithdraw = async(e: any) => {
     e.preventDefault()
     setIsPending(true)
     setError(null)
@@ -48,10 +49,14 @@ export default function Withdraw({userDoc}: any | {doc: any}) {
 
     // update balance
     try {
-      updateBalance(userDoc, data.amount, data.wallet)
+      await updateBalance(userDoc, data.amount, data.wallet)
+      setIsSuccess(true)
+      setIsPending(false)
+      setTimeout(() => setIsSuccess(false), 4000)
     } catch (error: any) {
       setError(error.message)
     }
+
     setIsPending(false)
     setAmount(0)
     setWallet('')
@@ -63,12 +68,14 @@ export default function Withdraw({userDoc}: any | {doc: any}) {
   return (
     <div className={s.ctn}>
       <h1>Withdraw</h1>
+      <p className={s.note}>Please note! Min withdrawal is $20 and might take up to 24 hours before approval</p>
 
       <form className={s.form}>
       <TextField label="Amount" InputLabelProps={{ shrink: true }} type="number" onChange={e => setAmount(e.target.value)} value={amount}/>
       <TextField label="Wallet" InputLabelProps={{ shrink: true }} type="text" onChange={e => setWallet(e.target.value)} value={wallet}/>
-          {!isPending && <button style={{...overwrite}} className='bigBtn full' onClick={handleWithdraw}>Withdraw</button>}
-          {isPending && <button style={{...overwrite}} className='bigBtn full'>Loading...</button>}
+          {!isPending && !isSuccess && <button style={{...overwrite}} className='bigBtn full' onClick={handleWithdraw}>Withdraw</button>}
+          {isPending && !isSuccess && <button style={{...overwrite}} className='bigBtn full load'>Loading...</button>}
+          {!isPending && isSuccess && <button style={{...overwrite, backgroundColor: "#edfff4"}}  className='bigBtn full'>Withdrawal Successful</button>}
           {error && <p className='formError'>{error}</p>}
       </form>
     </div>
@@ -79,6 +86,9 @@ export default function Withdraw({userDoc}: any | {doc: any}) {
 const overwrite = {
   background: "none",
   border: "1px solid #000000",
+  padding: "20px",
+  fontWeight: "300",
+  fontSize: "1.2rem",
 }
 
 
